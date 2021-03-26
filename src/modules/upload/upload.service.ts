de670d454ee0384325f7140ce1a5a98bbfc0807c
intelrug <intelrug@gmail.com>
2021-03-27 00:36:55 +0300

    fix(upload): update gm threads limit

--- src/modules/upload/upload.service.ts
+++ src/modules/upload/upload.service.ts
@@ -212,7 +212,13 @@ export class UploadService {
   }
 
   async compressImage(gmInstance: gm.State): Promise<CompressedPicture> {
-    const m = gmInstance.noProfile().setFormat('jpeg').resize(512, 512).quality(90).limit('memory', '900M');
+    const m = gmInstance
+      .noProfile()
+      .setFormat('jpeg')
+      .resize(512, 512)
+      .quality(90)
+      .limit('memory', '900M')
+      .limit('threads', '1');
     const mBuffer = await this.gmToBuffer(m);
 
     const s = gm(mBuffer).noProfile().setFormat('jpeg').resize(128, 128).quality(98);
