d79e6cb497587bef248fdd751c15e427b46f39f1
intelrug <intelrug@gmail.com>
2021-10-03 16:50:33 +0300

    feat: serve uploads directory

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
   const PORT = parseInt(process.env.PORT || '7700', 10);
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+
+  app.useStaticAssets(join(process.cwd(), 'uploads'), {
+    prefix: '/cdn/',
+  });
+
   await app.listen(PORT);
 }
 bootstrap();
