1d16ea03cc8605b07ef079b9b0507c4415375a0b
intelrug <intelrug@gmail.com>
2024-12-04 13:34:14 +0500

    feat: create version query

--- src/app.resolver.ts
+++ src/app.resolver.ts
@@ -1,6 +1,8 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { UseFilters } from '@nestjs/common';
 import { HttpExceptionFilter } from '@modules/common/filters/http-exception.filter';
+import { join } from 'path';
+import { readFileSync } from 'fs';
 
 @UseFilters(HttpExceptionFilter)
 @Resolver('App')
@@ -8,5 +10,16 @@ export class AppResolver {
   @Query(() => String)
   getHello(): string {
     return 'Hello World!';
+  }
+
+  @Query(() => String)
+  async version(): Promise<string> {
+    try {
+      const versionFile = join(__dirname, '..', 'version.json');
+      const versionData = JSON.parse(readFileSync(versionFile, 'utf8'));
+      return versionData.version;
+    } catch {
+      return 'development';
+    }
   }
 }
